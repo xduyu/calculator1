3d8ebe49f9930ab1720bdf5f1da5011a52fa9598
art <147410114+xduyu@users.noreply.github.com>
2024-10-17 18:10:42 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// // // // getElementById()
+
+// // // const p1 = document.getElementById("first")
+
+// // // console.log(p1)
+
+
+
+// // // //getElementsByClassName
+
+// // // const p2a3 = document.getElementsByClassName("ele")
+// // // console.log(p2a3)
+
+
+// // // //getElementByTagName
+
+
+// // // const elemntsTag = document.getElementsByTagName("p")
+// // // console.log(elemntsTag)
+
+
+
+// // // // выбирает по css селектору  
+// // // // Тоесть и по классу
+
+
+
+// // // const p12 = document.querySelector(".ele")
+
+
+
+// // // console.log(p12)
+
+// // // const p13 = document.querySelectorAll(".ele")
+
+// // // console.log(p13)
+
+
+
+// // const bookList = document.querySelector(".books")
+
+
+// // console.log(bookList.childNodes)
+// // console.log(bookList.children)
+
+
+// // const bookTwo = document.querySelector(".book02")
+
+// // console.log(bookTwo)
+
+
+// // console.log(bookTwo.nextSibling)
+
+// // console.log(bookTwo.previousSibling)
+
+// // console.log(bookTwo.nextElementSibling)
+
+// // console.log(bookTwo.previousElementSibling)
+
+
+// const text = document.querySelector(".text")
+// // console.log(text.innerHTML)
+// // console.log(text.textContent)
+// // console.log(text.outerHTML)
+
+
+
+// // text.innerHTML = "hello, world!";
+// // text.textContent = "Hello, world!";
+
+// text.hidden = true
+
+
+// console.log(text.hidden)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const inputData = document.getElementById("inputSquare")
+const btnSquare = document.getElementById("btn-square")
+const btnPeremitr = document.getElementById("btn-peremitr")
+const btnAll = document.getElementById("btn-all")
+const resSquare = document.getElementById("res-square")
+const resPerimtr = document.getElementById("res-perimtr")
